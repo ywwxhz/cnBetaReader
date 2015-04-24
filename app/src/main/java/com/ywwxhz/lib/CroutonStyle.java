6015db3e33fbe36d7e052f183c24c3f3dec87df8
远望の无限 <ywwxhz@gmail.com>
2015-04-24 09:34:12 +0800

    fix some bugs

--- app/src/main/java/com/ywwxhz/lib/CroutonStyle.java
+++ app/src/main/java/com/ywwxhz/lib/CroutonStyle.java
@@ -1,5 +1,7 @@
 package com.ywwxhz.lib;
 
+import java.util.HashMap;
+
 import de.keyboardsurfer.android.widget.crouton.Style;
 
 /**
@@ -12,8 +14,21 @@ public class CroutonStyle {
     public static Style CONFIRM;
     public static void buildStyleInfo(int color){
         INFO = new Style.Builder(Style.INFO).setBackgroundColorValue(color&0xD0FFFFFF).build();
+        styleMap.put("info",INFO);
     }
     public static void buildStyleConfirm(int color){
         CONFIRM = new Style.Builder(Style.CONFIRM).setBackgroundColorValue(color&0xD0FFFFFF).build();
+        styleMap.put("info",CONFIRM);
     }
+    private static HashMap<String,Style> styleMap = new HashMap<>(3);
+
+    public static Style getStyle(String key){
+        Style style = styleMap.get(key);
+        if(style!=null){
+            return style;
+        }else{
+            return Style.ALERT;
+        }
+    }
+
 }
